d67ae7c42644956f813e7c11ca708ddfd5c391cf
xuhao <642508924@qq.com>
2020-12-16 17:37:20 +0800

    Update formatDate.js

--- groceryStore/待整理/formatDate.js
+++ groceryStore/待整理/formatDate.js
@@ -1,9 +1,17 @@
+/*
+ * @Author: 徐浩
+ * @Date: 2020-12-16 16:30:20
+ * @LastEditTime: 2020-12-16 17:36:33
+ * @LastEditors: Please set LastEditors
+ * @Description: 将时间戳转换成日期形式
+ * @FilePath: kuaidaowanlilai-xyz.github.io\groceryStore\待整理\formatDate.js
+ */
 /**
  * 将时间戳转换成日期形式
  * @param {Number} timeStamp 时间戳
  * @param {String} type 输出的日期格式 yyyy-MM-dd HH:mm:ss
  */
-const formatDate = (timeStamp, type) => { 
+const formatDate = (timeStamp = new Date().getTime(), type = 'yyyy-MM-dd HH:mm:ss') => { 
   let time = new Date(timeStamp)
   let year = time.getFullYear() //取得4位数的年份
   let month = time.getMonth()+1 //取得日期中的月份，其中0表示1月，11表示12月
@@ -16,19 +24,15 @@ const formatDate = (timeStamp, type) => {
   let minuteB = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes() //返回日期中的分钟数（00到59）
   let second = time.getSeconds() //返回日期中的秒数（0到59）
   let secondB = time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds() //返回日期中的秒数（00到59）
-  if(type){
-    return type.replace(/y{4}/g, year)
-                .replace(/M{2}/g, monthB)
-                .replace(/d{2}/g, dateB)
-                .replace(/H{2}/g, hourB)
-                .replace(/m{2}/g, minuteB)
-                .replace(/s{2}/g, secondB)
-                .replace(/M/g, month)
-                .replace(/d/g, date)
-                .replace(/H/g, hour)
-                .replace(/m/g, minute)
-                .replace(/s/g, second)
-  }else{
-    return `${year}-${month}-${date}`
-  }
+  return type.replace(/y{4}/g, year)
+              .replace(/M{2}/g, monthB)
+              .replace(/d{2}/g, dateB)
+              .replace(/H{2}/g, hourB)
+              .replace(/m{2}/g, minuteB)
+              .replace(/s{2}/g, secondB)
+              .replace(/M/g, month)
+              .replace(/d/g, date)
+              .replace(/H/g, hour)
+              .replace(/m/g, minute)
+              .replace(/s/g, second)
 }
